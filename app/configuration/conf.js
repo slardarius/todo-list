69badd60eda2f15726c289aa8d4392f1d5185a33
Artem <artem.zapaschikov@agilie.com>
2019-12-10 22:47:27 +0200

    Svalte

--- app/configuration/conf.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    isProduction: process.env.NODE_ENV === 'production',
-    serverPort: process.env.PORT || 3000,
-    mongoHost: process.env.DB_HOST,
-    mongoDbName: process.env.DATA_BASE_NAME,
-};
