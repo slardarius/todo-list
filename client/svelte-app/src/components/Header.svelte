7c52379b058c04b0db1bf0a87b5b37948259bf01
Artem <artem.zapaschikov@agilie.com>
2019-12-10 23:25:26 +0200

    - implemented first component on Svelte

---
+++ client/svelte-app/src/components/Header.svelte
@@ -0,0 +1,16 @@
+<header calss="todo-list__header">
+    <nav>
+        <ul class="todo-list__nav">
+            <li> Home </li>
+            <li> Contact info</li>
+        </ul>
+    </nav>
+</header>
+
+<style>
+
+</style>
+
+<script>
+
+</script>
